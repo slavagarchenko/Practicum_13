869f9f228f727736cad996ec8903fae5fb979f2c
slavagarchenko <cvgarchenko@gmail.com>
2025-10-06 11:53:22 +0700

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,6 +1,11 @@
-def print_fibonacci_simple(n):
+def print_fibonacci_sequence(n):
     """
-    
+    Prints the first N numbers of the Fibonacci sequence.
+
+    Args:
+        n (int): the number if Fibonacci numbers to print
+    Returns:
+        None
     """
     print(f"Первые {n} чисел последовательности Фибоначчи:")
     
@@ -12,18 +17,16 @@ def print_fibonacci_simple(n):
     prev1 = 1
     prev2 = 1
     
-    # Генерируем остальные числа
     for i in range(3, n + 1):
         current = prev1 + prev2
         print(f", {current}", end="")
         
-        # Обновляем предыдущие числа для следующей итерации
         prev1, prev2 = prev2, current
-    
-    print()  # Завершаем вывод
 
-# Демонстрация работы:
-numbers_to_print = [1, 2, 5, 8]
-for num in numbers_to_print:
-    print_fibonacci_simple(num)
-    print("---")
+number = int(input())
+
+while number<0:
+    print("Введите положительное число порядка")
+    number = int(input())
+
+print_fibonacci_sequence(number)
