5e80835d515e95d2bbf81e49b4c0a67fbc94a8a7
slavagarchenko <cvgarchenko@gmail.com>
2025-10-06 11:26:46 +0700

    Create ex_1.py

---
+++ ex_1.py
@@ -0,0 +1,30 @@
+def count_vowels_consonants(sentence):
+  """
+  The function accepts a sentence in Russian 
+  and prints the total number of vowels and consonants
+
+  Args:
+    sentence (str): analysed sentence
+  Returns:
+    None
+  Prints:
+    vowel_count (int) - number of vowels in sentence
+    consonant_count (int) - number of consonants in sentence
+  """
+  sentence lower = sentence.lower()
+  
+  vowels = set('аеёиоуыэюя')
+  consonants = set('бвгджзйклмнпрстфхцчшщ')
+  
+  vowel_count = 0
+  consonant_count = 0
+
+  for char in sentence_lower:
+    if char.isalpha():
+      if char in vowels:
+        vowel_count +=1
+      elif char in consonants:
+        consonant_count +=1
+
+  print(f"Количество гласных букв: {vowel_count}")
+  print(f"Количество согласных букв: {consonant_count}")
