602b2e65bfe9b55bbab00f61b946b4fc10e49640
slavagarchenko <cvgarchenko@gmail.com>
2025-10-06 17:43:21 +0700

    Create ex_6.py

---
+++ ex_6.py
@@ -0,0 +1,19 @@
+def check_message_length(message):
+  """
+  Check and adjust message length to comply with SMS character limits
+
+  Args:
+    message (str): the text message to be checked for length comliance
+
+  Returns:
+    str: the original message if under 160 characters, 
+          or truncated to first 160 characters if over the limit
+  """
+  if len(message)<=160:
+    return message
+  else:
+    return message[:160]
+
+message = input (Введите текст сообщения: )
+
+check_message_length(message)
