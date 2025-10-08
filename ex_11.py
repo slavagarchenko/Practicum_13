ec6db0eaaa81636d1e6c4d0e5ac6825208579920
slavagarchenko <cvgarchenko@gmail.com>
2025-10-08 09:30:42 +0700

    Update ex_11.py

--- ex_11.py
+++ ex_11.py
@@ -1,14 +1,25 @@
 def prime(n):
+    """
+    Check if a number is prime
+
+    Args:
+        n (int): the number to check for primality
+    Returns:
+        bool: True if the number is prime, False otherwise
+    """
     if n<2:
         return False
     if n==2:
         return True
     if n%2 == 0:
         return False
-    for i in range(3, int(n**.5)+1,2):
+    for i in range(3, int(n**.5)+1, 2):
         if n%i==0:
             return False
     return True
 
 number = int(input("Введите число: "))
-prime(number)
+
+for i in range(1,number+1):
+    if prime(i):
+        print(i, end = " ")
